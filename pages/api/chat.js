42e19f8724a0c52c4103d012bfc9bac0ad25f453
ThatLukinhasGuy <139662282+thatlukinhasguy1@users.noreply.github.com>
2023-10-15 04:21:31 -0300

    Update and rename chat.ts to chat.js

---
+++ pages/api/chat.js
@@ -0,0 +1,24 @@
+const { Configuration, OpenAI, ChatCompletionRequestMessageRoleEnum } = require("openai");
+const { NextApiRequest, NextApiResponse } = require("next");
+
+const openai = new OpenAI({
+  apiKey: '',
+  baseURL: 'https://neuroapi.host/v1'
+});
+
+async function chatHandler(req, res) {
+  const completion = await openai.createChatCompletion({
+    model: "gpt-4",
+    messages: [
+      {
+        role: ChatCompletionRequestMessageRoleEnum.System,
+        content: "You are a helpful assistant.",
+      },
+      
+    ].concat(req.body.messages),
+    temperature: 0,
+  });
+  res.status(200).json({ result: completion.data.choices[0].message });
+}
+
+module.exports = chatHandler;
