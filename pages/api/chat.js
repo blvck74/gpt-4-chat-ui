349e5fe5ff09f2761420669c5461ed4d0b3b6c98
blvck74 <85095146+blvck74@users.noreply.github.com>
2024-12-13 03:29:48 +0300

    Update chat.js

--- pages/api/chat.js
+++ pages/api/chat.js
@@ -3,7 +3,7 @@ const { NextApiRequest, NextApiResponse } = require("next");
 
 const openai = new OpenAI({
   apiKey: 'sk-gfEzUwwBc2ybxPeg56Ac07Ac371c495eB7E52f09883e32F3',
-  baseURL: 'https://neuroapi.host/v1/chat/'
+  baseURL: 'https://neuroapi.host/v1/chat/completions'
 });
 
 async function chatHandler(req, res) {
